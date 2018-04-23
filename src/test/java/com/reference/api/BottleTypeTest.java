83d19d42c25d54bace7122d5e2683c91b058a34d
ludovic <ludovic.millon.info@gmail.com>
2018-04-23 16:05:48 +0200

    Fin des tests

--- src/test/java/com/reference/api/BottleTypeTest.java
+++ src/test/java/com/reference/api/BottleTypeTest.java
@@ -5,10 +5,7 @@ import com.reference.api.repository.*;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
@@ -19,7 +16,9 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.json.JacksonJsonParser;
 
 import javax.naming.Name;
@@ -38,7 +37,7 @@ import static org.apache.http.HttpStatus.SC_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BottleTypeTest {
 
     /***
@@ -76,7 +75,7 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_200_On_create_user() throws IOException, URISyntaxException, Exception {
+    public void t01_should_200_On_create_user() throws IOException, URISyntaxException, Exception {
         HttpPost request = new HttpPost(new URL("http://localhost:" + 8080 + "/api/user/?username=ludo&password=123").toURI());
 
         request.setHeader("Accept", "application/json, text/plain, */*");
@@ -87,7 +86,7 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_create_bottletype() throws IOException, URISyntaxException, Exception {
+    public void t02_should_create_bottletype() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("ludo","123");
         HttpPost request = new HttpPost(new URL("http://localhost:" + 8080 + "/api/bottletype/").toURI());
 
@@ -121,7 +120,7 @@ public class BottleTypeTest {
 
 
     @Test
-    public void should_200_On_Existing_BottleType() throws IOException, URISyntaxException, Exception {
+    public void t03_should_200_On_Existing_BottleType() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("ludo","123");
 
         HttpUriRequest request = new HttpGet(new URL("http://localhost:" + 8080 + "/api/bottletype/").toURI());
@@ -131,7 +130,7 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_200_On_BottleType_All() throws IOException, URISyntaxException, Exception {
+    public void t04_should_200_On_BottleType_All() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("ludo","123");
         HttpUriRequest request = new HttpGet(new URL("http://localhost:" + 8080 + "/api/bottletype/all").toURI());
         request.setHeader("Authorization", "Bearer " + token);
@@ -140,7 +139,7 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_200_On_Updating_BottleType() throws IOException, URISyntaxException, Exception {
+    public void t05_should_200_On_Updating_BottleType() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("admin","admin");
         HttpPut request = new HttpPut(new URL("http://localhost:" + 8080 + "/api/bottletype/").toURI());
 
@@ -176,7 +175,7 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_200_On_create_compartment() throws IOException, URISyntaxException, Exception {
+    public void t06_should_200_On_create_compartment() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("ludo","123");
 
         HttpPost request = new HttpPost(new URL("http://localhost:" + 8080 + "/api/compartment/").toURI());
@@ -208,7 +207,7 @@ public class BottleTypeTest {
 
 
     @Test
-    public void compartment() throws IOException, URISyntaxException, Exception {
+    public void t07_should_200_On_update_compartment() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("ludo","123");
         HttpPut request = new HttpPut(new URL("http://localhost:" + 8080 + "/api/compartment/").toURI());
 
@@ -239,7 +238,7 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_200_On_create_bottle() throws IOException, URISyntaxException, Exception {
+    public void t08_should_200_On_create_bottle() throws IOException, URISyntaxException, Exception {
         String token = obtainAccessToken("ludo","123");
 
         HttpPost request = new HttpPost(new URL("http://localhost:" + 8080 + "/api/bottle/").toURI());
@@ -276,24 +275,85 @@ public class BottleTypeTest {
     }
 
     @Test
-    public void should_200_On_update_bottle() throws IOException, URISyntaxException, Exception {
+    public void t09_should_200_On_update_bottle() throws IOException, URISyntaxException, Exception {
+        String token = obtainAccessToken("ludo","123");
+        HttpPut request = new HttpPut(new URL("http://localhost:" + 8080 + "/api/bottle/").toURI());
 
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Authorization", "Bearer " + token);
+
+        ObjectMapper mapper = new ObjectMapper();
+        User u = new User("ludo","");
+        u.setId(Integer.toUnsignedLong(1));
+
+        BottleType BT = new BottleType("blabla", 1850,"Rhone", "rouge",true);
+        BT.setId(Integer.toUnsignedLong(1));
+
+        Compartment C = new Compartment("A2", u);
+        C.setId(new Long(1));
+
+        Bottle obj = new Bottle(u, BT, C, 188, null);
+        obj.setId(Integer.toUnsignedLong(1));
+
+        request.setEntity(new StringEntity(mapper.writeValueAsString(obj), "UTF-8"));
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(SC_OK);
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        String result = rd.readLine();
+
+        JsonNode rootNode = mapper.readTree(result);
+
+        assertEquals(rootNode.get("nbBottles").asText(), "188");
     }
 
     @Test
-    public void should_200_On_delete_bottle() throws IOException, URISyntaxException, Exception {
+    public void t10_should_200_On_delete_bottle() throws IOException, URISyntaxException, Exception {
+        String token = obtainAccessToken("ludo","123");
 
+        HttpDelete request = new HttpDelete(new URL("http://localhost:" + 8080 + "/api/bottle/1").toURI());
+
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Authorization", "Bearer " + token);
+
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(SC_OK);
     }
 
     @Test
-    public void should_200_On_delete_compartment() throws IOException, URISyntaxException, Exception {
+    public void t11_should_200_On_delete_compartment() throws IOException, URISyntaxException, Exception {
+        String token = obtainAccessToken("ludo","123");
 
+        HttpDelete request = new HttpDelete(new URL("http://localhost:" + 8080 + "/api/compartment/1").toURI());
+
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Authorization", "Bearer " + token);
+
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(SC_OK);
     }
 
     @Test
-    public void should_200_On_delete_bottletype() throws IOException, URISyntaxException, Exception {
+    public void t12_should_200_On_delete_bottletype() throws IOException, URISyntaxException, Exception {
+        String token = obtainAccessToken("ludo","123");
 
+        HttpDelete request = new HttpDelete(new URL("http://localhost:" + 8080 + "/api/bottletype/1").toURI());
+
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Authorization", "Bearer " + token);
+
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(SC_OK);
     }
-
 
 }
